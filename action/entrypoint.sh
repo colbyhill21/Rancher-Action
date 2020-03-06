98352400778e437c384ebfba319e91278d0eaa9b
Colby Hill <32140554+colbyhill21@users.noreply.github.com>
2020-03-06 12:25:27 -0500

    Update entrypoint.sh

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -1,13 +1,8 @@
 #!/bin/sh -l
 
-
 token="$INPUT_TOKEN"
 context="$INPUT_CONTEXT"
 url="$INPUT_URL"
 
-echo "$token"
-echo "$context"
-echo "$url"
-
-# rancher login --token token --context context url
-# rancher kubectl $*
+rancher login --token $token --context $context $url
+rancher kubectl $*
